d7c1b688281fe6f4242b694f8223ade66e230703
hamdolly <ahamhmo@gmail.com>
2025-06-09 20:32:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+import express from "express"
+import cors from "cors"
+import * as info from "./info.js"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("It is work!")
+})
+
+app.post('/login/:e_No/:password', async (req, res) => {
+    try {
+        // var data = req.body
+        var { e_No, password } = req.params
+        res.status(200).send((await info.login(e_No, password)).result)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.post("/receipt", async (req, res) => {
+    try {
+        var data = req.body
+        if ((await info.checkCustomerNo(data[0].customer_No)).check == 1) {
+            var receipt = await info.addNewReceipt(data[0].e_No, data[0].customer_No, data[0].ep, data[0].date, data[0].supervisor)
+
+            res.status(200).send(receipt)
+        } else {
+            res.status(400).send([
+                {
+                    "affectedRows": 0,
+                    "Message": "!رقم هذا العميل مضاف بالفعل",
+                    "success": "0"
+                }
+            ])
+        }
+        // res.send(data)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.get('/employee/newcustomers/quantity', async (req, res) => {
+    try {
+        res.status(200).send(await info.getAllEmployeesReceiptsQuantity())
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.get('/employee/newcustomers/quantity/ID/:ID', async (req, res) => {
+    try {
+        let { ID } = req.params
+        res.status(200).send(await info.getEmployeeReceiptsQuantity(ID))
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.get('/employee/newcustomers/quantity/date/:date', async (req, res) => {
+    try {
+        let { date } = req.params
+        res.status(200).send(await info.getReceiptsQuantityByDate(date))
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.get('/employee/newcustomers/quantity/:ID/:date', async (req, res) => {
+    try {
+        let { ID, date } = req.params
+        res.status(200).send(await info.getEmployeeReceiptsQuantityByDate(ID, date))
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+var port = 3100
+app.listen(port || process.env.PORT, () => console.log(`Started a port: ${port}`))
---
+++ info.js
@@ -0,0 +1,145 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database: "employees"
+
+    host: 'sql12.freesqldatabase.com',
+    port: 3306,
+    user: 'sql12763292',
+    password: 'tyvfHRcTGP',
+    database: 'sql12763292'
+}).promise()
+
+export const checkCustomerNo = async (customer_No) => {
+    let [result] = await pool.query(`SELECT COUNT(customer_No) AS count FROM receipt WHERE customer_No = ${customer_No}`)
+    var check = 0
+    result[0].count >= 1 ? check = 0 : check = 1
+    return { check }
+}
+
+export const login = async (e_No, password) => {
+    var data = []
+    var [result] = await pool.query(`SELECT * FROM employees WHERE e_No = ?`, [e_No])
+    result[0].position == "Supervisor" ?
+        data.push(
+            {
+                "ID": result[0].e_No,
+                "passowrd": result[0].passowrd
+            }
+        )
+        :
+        data.push(
+            {
+                "Message": "employee number or password is not currect!",
+                "success": 0
+            }
+        )
+    return {
+        result: data[0].passowrd == password ? data : "ID or password is wrong!"
+    }
+}
+
+export const addNewReceipt = async (e_No, customer_No, ep, date, supervisor) => {
+    let [result] = await pool.query(`INSERT INTO receipt (e_No, customer_No, earning_point, datE, supervisor) 
+    VALUES (?, ?, ?, ?, ?)`, [e_No, customer_No, ep, date, supervisor])
+    return [
+        {
+            "affectedRows": result.affectedRows,
+            "success": 1
+        }
+    ]
+}
+
+export const getAllEmployeesReceiptsQuantity = async () => {
+    var data = []
+    var [employee] = await pool.query(`SELECT * FROM employees`)
+    var result, resultY, resultN;
+    for (var i = 0; i < employee.length; i++) {
+        [result] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND BLOCK = 0`);
+        [resultY] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND earning_point = 1 AND BLOCK = 0`);
+        [resultN] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND earning_point = 0 AND BLOCK = 0`);
+
+        data.push(
+            {
+                "ID": employee[i].ID,
+                "employee": employee[i].AName,
+                "pointsY": resultY[0].receipts,
+                "pointsN": resultN[0].receipts,
+                "quantity": result[0].receipts,
+            }
+        )
+    }
+    return data
+}
+
+export const getEmployeeReceiptsQuantity = async (ID) => {
+    var data = []
+    var data = []
+    var [employee] = await pool.query(`SELECT * FROM employees WHERE ID = ${ID}`)
+    var result, resultY, resultN;
+    for (var i = 0; i < employee.length; i++) {
+        [result] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND BLOCK = 0`);
+        [resultY] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND earning_point = 1 AND BLOCK = 0`);
+        [resultN] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND earning_point = 0 AND BLOCK = 0`);
+
+        data.push(
+            {
+                "ID": employee[i].ID,
+                "employee": employee[i].AName,
+                "pointsY": resultY[0].receipts,
+                "pointsN": resultN[0].receipts,
+                "quantity": result[0].receipts,
+            }
+        )
+    }
+    return data
+}
+
+export const getEmployeeReceiptsQuantityByDate = async (ID, date) => {
+    var data = []
+    var [employee] = await pool.query(`SELECT * FROM employees WHERE ID = ${ID}`)
+    var result, resultY, resultN;
+    for (var i = 0; i < employee.length; i++) {
+        [result] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${ID} AND datE = '${date}' AND BLOCK = 0`);
+        [resultY] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${ID} AND datE = '${date}' AND earning_point = 1 AND BLOCK = 0`);
+        [resultN] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${ID} AND datE = '${date}' AND earning_point = 0 AND BLOCK = 0`);
+
+        data.push(
+            {
+                "ID": employee[i].ID,
+                "employee": employee[i].AName,
+                "pointsY": resultY[0].receipts,
+                "pointsN": resultN[0].receipts,
+                "quantity": result[0].receipts,
+            }
+        )
+    }
+    return data
+}
+
+export const getReceiptsQuantityByDate = async (date) => {
+    var data = []
+    var [employee] = await pool.query(`SELECT * FROM employees`)
+    var result, resultY, resultN;
+    for (var i = 0; i < employee.length; i++) {
+        [result] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND datE = '${date}' AND BLOCK = 0`);
+        [resultY] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND datE = '${date}' AND earning_point = 1 AND BLOCK = 0`);
+        [resultN] = await pool.query(`SELECT COUNT(*) AS receipts FROM receipt WHERE e_No = ${employee[i].ID} AND datE = '${date}' AND earning_point = 0 AND BLOCK = 0`);
+
+        data.push(
+            {
+                "ID": employee[i].ID,
+                "employee": employee[i].AName,
+                "pointsY": resultY[0].receipts,
+                "pointsN": resultN[0].receipts,
+                "quantity": result[0].receipts,
+            }
+        )
+    }
+    return data
+}
+
+export const test = () => console.log("info module is work.")
