d51b38b1f6d07cfe17960fabf5f0b88db0536172
hamdolly <ahamhmo@gmail.com>
2025-06-10 19:11:34 +0300

    8th commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from "express"
+import express from 'express';
 import * as info from "./info.js"
-import cors from "cors"
+import cors from 'cors'
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("It is work!")
